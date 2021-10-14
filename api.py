fb181191a981efd87a85fda78a0daa904b422154
Weber <36941261+Weber12321@users.noreply.github.com>
2021-10-15 02:50:28 +0800

    first

---
+++ api.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import uvicorn
+
+from model import RunModel
+from fastapi import FastAPI, File, UploadFile, status, Query
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+app = FastAPI(title="Bumble Bee", description="Predict LD50 value by linear regression.")
+
+@app.post("/upload/")
+def upload_csv(test_size: float = Query(...),
+               csv_file: UploadFile = File(...)):
+    dataframe = pd.read_csv(csv_file.file)
+    try:
+        m = RunModel(dataframe, test_size)
+        cod, mse = m.run()
+        output = {
+            'Coefficient of determination' : cod,
+            'Mean square error' : mse
+        }
+        return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(output))
+    except Exception as e:
+        output = {
+            'error message' : e
+        }
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=jsonable_encoder(output))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', debug=True)
